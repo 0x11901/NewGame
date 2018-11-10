b3d90292e853e5a4a9efc1b27edd89e5d6d14a4d
王靖凯 <wjk930726@live.com>
2018-11-11 04:09:11 +0800

    Gross hack because Marcus doesn't know how to code

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -4,12 +4,36 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    // Use this for initialization
-    private void Start()
+    public GameObject DesignTemplate;
+    private Vector3 _size;
+
+
+    private void Awake()
     {
+//        _size = DesignTemplate.transform.GetComponent<MeshFilter>().mesh.bounds.size;
+        _size = new Vector3(0.05207656f, 0.06724269f, 0.03182981f);
     }
 
-    // Update is called once per frame
+    /// <summary>
+    /// Use this for initialization
+    /// </summary>
+    private void Start()
+    {
+        for (var i = 0; i < 18; ++i)
+        {
+            for (var j = 0; j < 2; ++j)
+            {
+                var card = Instantiate(DesignTemplate);
+                var startPosition = DesignTemplate.transform.position;
+                card.transform.position = new Vector3(startPosition.x + _size.x * 0.5f + i * _size.x, startPosition.y,
+                    startPosition.z);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
     private void Update()
     {
     }
