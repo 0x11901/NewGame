e967d29a983e8bb41e034eb72d4bd20f3c5e1f60
王靖凯 <wjk930726@live.com>
2018-11-25 03:27:49 +0800

    minor changes

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -146,6 +146,7 @@ public class NewBehaviourScript : MonoBehaviour
         _total = 84;
         ShowHands();
 
+        // self
         var hand = Instantiate(_hand);
         var handAnimation = hand.GetComponent<Animation>();
         handAnimation.wrapMode = WrapMode.Once;
@@ -159,6 +160,18 @@ public class NewBehaviourScript : MonoBehaviour
 
         yield return new WaitForSeconds(handAnimation.clip.length);
         Destroy(hand);
+
+        // left
+//        var hand = Instantiate(_hand, new Vector3(0.05f, 0.036f, 0.225f), new Quaternion(0f, 0.0f, 90f, 0));
+//
+//        var handAnimation = hand.GetComponent<Animation>();
+//        handAnimation.wrapMode = WrapMode.Once;
+//
+//        yield return Bar(new Vector3(-0.125f, 0.111f, 0.261f), new Quaternion(-0.052f, 89.858f, 177.683f, 0),
+//            1.0f);
+//
+//        yield return new WaitForSeconds(handAnimation.clip.length);
+//        Destroy(hand);
     }
 
     private IEnumerator Bar(Vector3 vector3, Quaternion quaternion, float dt)
