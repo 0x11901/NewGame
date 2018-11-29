7d53321d3bef21cf100011720c8f833ff0736ed7
王靖凯 <wjk930726@live.com>
2018-11-30 02:51:30 +0800

    For the sake of my sanity, just ignore this...

---
+++ Assets/Scripts/Hit.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Hit : MonoBehaviour
+{
+    [SerializeField] private GameObject _box;
+
+    // Use this for initialization
+    private void Start()
+    {
+        for (var i = 0; i < 5; i++)
+        {
+            for (var j = 0; j < 5; j++)
+            {
+                var box = Instantiate(_box);
+                box.transform.position = new Vector3(-2.0f + i, 0.5f + j, 4.5f);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
