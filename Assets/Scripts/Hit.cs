236b7edf49b662430363171acc39188aaf728a29
王靖凯 <wjk930726@live.com>
2018-12-01 21:10:20 +0800

    Best commit ever

--- Assets/Scripts/Hit.cs
+++ Assets/Scripts/Hit.cs
@@ -5,9 +5,15 @@ public class Hit : MonoBehaviour
     [SerializeField] private GameObject _box;
     [SerializeField] private GameObject _bullet;
 
+    private float _cd;
+    private float _margin;
+
     // Use this for initialization
     private void Start()
     {
+        _cd = 0.5f;
+        _margin = 0.5f;
+
         for (var i = 0; i < 5; i++)
         {
             for (var j = 0; j < 5; j++)
@@ -23,15 +29,22 @@ public class Hit : MonoBehaviour
     {
         if (Input.GetButton("Fire1"))
         {
-            var mainCamera = Camera.main;
-            var bullet = Instantiate(_bullet);
-            bullet.transform.position = mainCamera.transform.position;
+            _margin += Time.deltaTime;
 
-            var ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit))
+            if (_margin >= _cd)
             {
-                bullet.GetComponent<Rigidbody>().velocity = (hit.point - bullet.transform.position) * 10;
+                _margin = 0.0f;
+                var mainCamera = Camera.main;
+                if (mainCamera == null) return;
+
+                var ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit))
+                {
+                    var bullet = Instantiate(_bullet);
+                    bullet.transform.position = mainCamera.transform.position;
+                    bullet.GetComponent<Rigidbody>().velocity = (hit.point - bullet.transform.position) * 10;
+                }
             }
         }
     }
