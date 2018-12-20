44349a94aacdf36623c93be79f6eeda480b0aca4
0x11901 <wjk930726@live.com>
2018-12-20 15:21:10 +0800

    breathe, =, breathe

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    #region Unity
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+
+    #endregion
+}
