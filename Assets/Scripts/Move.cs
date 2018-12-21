a833cd2a723699a82071bd8b294295ca718404d5
0x11901 <wjk930726@live.com>
2018-12-21 16:59:23 +0800

    squash me

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -2,16 +2,42 @@
 
 public class Move : MonoBehaviour
 {
+    [SerializeField] private GameObject hero;
+    private float _speed;
+
     #region Unity
 
     // Start is called before the first frame update
     private void Start()
     {
+        _speed = 1f;
     }
 
     // Update is called once per frame
     private void Update()
     {
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            this.transform.Translate(Vector3.right * _speed * Time.deltaTime);
+        }
+        else if (Input.GetAxis("Horizontal") < 0)
+        {
+            this.transform.Translate(Vector3.left * _speed * Time.deltaTime);
+        }
+
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            this.transform.Translate(Vector3.forward * _speed * Time.deltaTime);
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            this.transform.Translate(Vector3.back * _speed * Time.deltaTime);
+        }
+
+        if (Input.GetButton("Fire1"))
+        {
+            print("fire!!!");
+        }
     }
 
     #endregion
