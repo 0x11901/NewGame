65f56ec1f832ff71e1938550a74ae6f7742679c2
0x11901 <wjk930726@live.com>
2018-12-29 16:44:28 +0800

    bifurcation

--- Assets/Scripts/Initialization.cs
+++ Assets/Scripts/Initialization.cs
@@ -2,27 +2,11 @@
 
 public class Initialization : MonoBehaviour
 {
-    [SerializeField] private GameObject floor;
-    private Vector3 _size;
-
     #region Unity
 
     // Start is called before the first frame update
     private void Start()
     {
-        for (var i = -50; i <= 50; i++)
-        {
-            for (var j = -50; j < 50; j++)
-            {
-                var f = Instantiate(floor);
-                if (_size == Vector3.zero)
-                {
-                    _size = GetObjectSize(f);
-                }
-
-                f.transform.position = new Vector3(0 + _size.x * i, 0, _size.z * j);
-            }
-        }
     }
 
     // Update is called once per frame
